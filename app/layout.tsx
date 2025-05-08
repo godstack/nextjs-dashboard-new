829f733ff748b9c5576b8120d51424da65f0f763
godstack <aleshkakrab@gmail.com>
2025-05-08 22:10:38 +0200

    completed chapter 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import clsx from 'clsx';
+
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body className={clsx(inter.className, 'antialiased')}>{children}</body>
     </html>
   );
 }
